42d900fc7487027475644d5e0bc4bc22651990dc
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-01-26 11:11:07 +0200

    Changed Racket position

--- src/Racket.cpp
+++ src/Racket.cpp
@@ -4,7 +4,7 @@
 Racket::Racket(){
     this->setSize(sf::Vector2f(100, 10));
     this->setFillColor(sf::Color::White);
-    this->setPosition(sf::Vector2f(100, 100));
+    this->setPosition(sf::Vector2f(1000/2+50, 700-50));
 }
 
 void Racket::update(float dt){
