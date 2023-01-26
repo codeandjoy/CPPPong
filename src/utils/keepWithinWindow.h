abe9a6122f81a1fe9f231ae8a06cec3b37d8e85d
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-01-26 20:20:51 +0200

    Fixed keepWithinWindow.h

--- src/utils/keepWithinWindow.h
+++ src/utils/keepWithinWindow.h
@@ -11,11 +11,11 @@ void keepWithinWindow(sf::RenderWindow *window, sf::Sprite *obj){
     // Left
     if(obj->getPosition().x < 0) obj->setPosition(0, obj->getPosition().y);
     // Right
-    else if(obj->getPosition().x + obj->getTexture()->getSize().x > wSizeX) obj->setPosition(wSizeX - obj->getTexture()->getSize().x, obj->getPosition().y);
+    if(obj->getPosition().x + obj->getTexture()->getSize().x > wSizeX) obj->setPosition(wSizeX - obj->getTexture()->getSize().x, obj->getPosition().y);
     // Top
-    else if(obj->getPosition().y < 0) obj->setPosition(obj->getPosition().x, 0);
+    if(obj->getPosition().y < 0) obj->setPosition(obj->getPosition().x, 0);
     // Bottom
-    else if(obj->getPosition().y + obj->getTexture()->getSize().y > wSizeY) obj->setPosition(wSizeY - obj->getTexture()->getSize().y, obj->getPosition().y);
+    if(obj->getPosition().y + obj->getTexture()->getSize().y > wSizeY) obj->setPosition(obj->getPosition().x, wSizeY - obj->getTexture()->getSize().y);
 }
 
 
